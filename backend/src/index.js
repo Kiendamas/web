e5a27e1cc9f0380d8bd83dbd4e68d92f5b8ebdef
mlobeto <mercedeslobeto@gmail.com>
2025-08-23 12:53:09 -0300

    forece re

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 3001;
 const env = process.env.NODE_ENV || 'development';
-const syncOptions = env === 'development' ? { alter: true } : {};
+const syncOptions = env === 'development' ? { force: true } : {};
 
 sequelize.sync(syncOptions).then(() => {
   app.listen(PORT, () => {
